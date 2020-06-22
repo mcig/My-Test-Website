4411557aa9d3565a3ab8fb9773d0cd4e1d7ffb56
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-22 23:43:50 +0300

    Added giphy api

--- HomePage/giphyApi.js
+++ HomePage/giphyApi.js
@@ -1,17 +1,29 @@
-function giphyInit(){
-  bindDraggie(".grid-image-item",true);
+function giphyInit() {
+  bindDraggie(".grid-image-item", true);
+}
+
+class Gif {
+  constructor(url, width, height) {
+    this.url = url;
+    this.width = parseInt(width) + 12;
+    this.height = parseInt(height) + 12;
+  }
+}
+
+async function asyncGiphyFetch(giphyApi) {
+  let response = await fetch(giphyApi);
+  let json = await response.json();
+  return new Gif(json.data.fixed_height_downsampled_url, json.data.fixed_height_downsampled_width,
+    json.data.fixed_height_downsampled_height);
 }
 
 //these 2 are done
-function giphyRandomFetcher(domImgElementStr){
+function giphyRandomFetcher(domImgElementStr) {
   const giphyApi = "http://api.giphy.com/v1/gifs/random?api_key=gUCtGBLQKcuEFL5bgR8PcgOgrevJcbz6&tag=g"
-  asyncGiphyFetch(giphyApi).then(result=>{
-    $(domImgElementStr).attr("src",result);
-  }).catch(err => $("body").prepend("<div class='alert alert-danger'>"+err+"</div>"));
-}
-
-async function asyncGiphyFetch(giphyApi){
-  let response = await fetch(giphyApi);
-  let json = await response.json();
-  return json.data.fixed_height_small_url;
+  let gifWidth;
+  let gifHeight;
+  asyncGiphyFetch(giphyApi).then(fetchedGif => {
+    $(domImgElementStr).attr("src", fetchedGif.url);
+    $(domImgElementStr.parentNode).css({"width":fetchedGif.width,"height":fetchedGif.height});
+  }).catch(err => $("body").prepend("<div class='alert alert-danger'>" + err + "</div>"));
 }

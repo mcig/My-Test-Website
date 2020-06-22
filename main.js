a7da8d2d421bdb2a4b7fb4ca0330e84ff317b56b
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-22 12:28:53 +0300

    Added my-cat-carousel

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 function FlickityTest() {
   const idFlickityCarousel = '#test-flickity-carousel';
-  const classFlickityCarouselImgs = '.carousel-cell img';
-
   $(idFlickityCarousel).flickity({
-    cellAlign: 'left',
-    contain: true
+    cellAlign: 'center',
+    wrapAround:true,
+    pageDots:true
   });
-
-  let flkty = $(idFlickityCarousel).data('flickity');
-  let $imgs = $(classFlickityCarouselImgs);
-
-  $(idFlickityCarousel).on('scroll.flickity', (event, progress) => flkty.slides.forEach((slide, i) => {
-    let img = $imgs[i];
-    let x = (slide.target + flkty.x) * -1 / 3;
-    img.style.transform = 'translateX( ' + x + 'px)';
-    })
-  );
 }
 
 $(document).ready(() => FlickityTest());

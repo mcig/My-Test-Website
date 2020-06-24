484848b331775f39129bf018967fa5d5982c02fd
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-24 13:12:55 +0300

    added the reload new gifs button jquery

--- HomePage/buttons-jquery.js
+++ HomePage/buttons-jquery.js
@@ -1,4 +1,10 @@
-function buttonsInit(){
+function buttonsInit() {
   //Relayout button
-  $("#relayoutButton").on("click",()=>$packeryGrid.packery('layout'));
+  $("#relayoutButton").on("click", () => $packeryGrid.packery('layout'));
+  //Reload Gifs button
+  $("#reloadGifsButton").on("click", () => {
+    $packeryGrid.find(".grid-image-item").each((i,gridItem) => {
+      giphyRandomFetcher(gridItem.childNodes[1]);
+    });
+  });
 }

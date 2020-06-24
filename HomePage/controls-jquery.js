59049cb1ad0b74bd88ad99faac5d8349327a7dc9
Mustafa Cig Gokpinar <cig-mustafa.2001@hotmail.com>
2020-06-24 22:13:25 +0300

    Added the jquery of isotope buttons

--- HomePage/controls-jquery.js
+++ HomePage/controls-jquery.js
@@ -1,14 +1,25 @@
 function controlsInit() {
   //Relayout button
   $("#relayoutButton").on("click", () => $packeryGrid.packery('layout'));
+
   //Reload Gifs button
   $("#reloadGifsButton").on("click", () => {
     $packeryGrid.find(".grid-image-item").each((i,gridItem) => {
       giphyRandomFetcher(gridItem.childNodes[1]);
     });
   });
+  
+  //Show All
+  $("#showAllButton").on("click", () => $packeryGrid.isotope({filter:'*'}));
+
+  //Filter Gifs
+  $("#filterGifsButton").on("click", () => $packeryGrid.isotope({filter:'.randGifItem'}));
+
+  //Filter Boxes
+  $("#filterBoxesButton").on("click", () => $packeryGrid.isotope({filter:'.grid-item'}));
+
   //Liked Gif pop-up
-  jqueryDoubleClickMobileFriendly(".randgifs");
+  jqueryDoubleClickMobileFriendly('.randgifImgs');
 }
 
 function jqueryDoubleClickMobileFriendly(strTarget){
